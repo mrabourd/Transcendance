46bbea3d73a15ac5794e972454f379a12ee09ad1
Marie-aimee RABOURDIN <mrabourd@bess-f1r1s3.clusters.42paris.fr>
2024-03-13 18:29:56 +0100

    new page only frontend

---
+++ test4/public/javascripts/register.js
@@ -0,0 +1,16 @@
+
+
+function register () {
+    fetch('http://127.0.0.1:8000/api/users/register/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username: "marie", email: "coucou@g.com", password: "Mi0)miou"})
+  }).then(res => res.json())
+    .then(res => console.log(res));
+};
+
+// recuperer les elements renvoyes par le formulaire et les envyer dans stringify
+// si elements ok (code reponse bonne) envoyer "successfull ou un truc du genre"
