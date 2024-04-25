1a7f27f51a8b9a9cf1e174a8a2e71c68d374d07a
Alice VEDRENNE <avedrenn@bess-f1r3s1.clusters.42paris.fr>
2024-04-25 15:09:48 +0200

    Fix refresh

--- datas/backend/hello_django/urls.py
+++ datas/backend/hello_django/urls.py
@@ -15,18 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include         
+from django.urls import path, include
 from users.views import UserRegistrationAPIView, CustomObtainTokenPairView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.views.decorators.csrf import csrf_exempt
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("", include("singlepage.urls")),      
+    path("", include("singlepage.urls")),
 	path('api/auth/', include('rest_framework.urls')),         # new
 	path('api/users/', include('users.urls')),
 ]
@@ -35,7 +32,7 @@ urlpatterns = [
 
 """ urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("singlepage.urls")),      
+    path("", include("singlepage.urls")),
 	#path('api/auth/', include('rest_framework.urls')),         # new
 	path('api/users/', include('users.urls')),
 
