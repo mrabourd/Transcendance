d5144c63df528468aefb230aa553de72e5de543d
avedrenn <avedrenn@42.student.fr>
2024-05-20 16:18:39 +0200

    Notifications from backend working

--- datas/frontend/public/javascripts/user.js
+++ datas/frontend/public/javascripts/user.js
@@ -15,6 +15,7 @@ export default class User {
     {
         return this._isConnected;
     }
+	
     set view(n)
     {
         this._view = n;
@@ -46,46 +47,6 @@ export default class User {
                 console.warn("Attaque CSRF");
                 return true;
             } */
-			this.notifyScoket = new WebSocket('wss://localhost:8443/ws/notify/?token=' + user.request.getJWTtoken()["access"] +'/'
-			);
-			
-			// on socket open
-			this.notifyScoket.onopen = function (e) {
-				console.log('Socket successfully connected.');
-			};
-			
-			// on socket close
-			this.notifyScoket.onclose = function (e) {
-				console.log('Socket closed unexpectedly');
-			};
-			
-			// on receiving message on group
-			this.notifyScoket.onmessage = function (e) {
-				const data = JSON.parse(e.data);
-				const message = data.message;
-				// Call the setMessage function to add the new li element
-				var newLi = document.createElement('li');
-			
-				// Create a new anchor element
-				var newAnchor = document.createElement('a');
-				newAnchor.className = 'dropdown-item text-wrap';
-				newAnchor.href = '#';
-				newAnchor.textContent = message;
-			
-				// Append the anchor element to the li element
-				newLi.appendChild(newAnchor);
-			
-				// Get the ul element with the id "notify"
-				var ulElement = document.getElementById('notify');
-			
-				// Append the new li element to the ul element
-				ulElement.appendChild(newLi);
-			
-				// getting object of count
-				count = document.getElementById('bellCount').getAttribute('data-count');
-				document.getElementById('bellCount').setAttribute('data-count', parseInt(count) + 1);
-			
-			};
 			
             
             return true;
